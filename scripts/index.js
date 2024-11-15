137bc4a70e72f4302b661b100cc623e6ce1d6477
Isaac Kennington <81638887+isaackengh@users.noreply.github.com>
2024-11-15 11:58:29 +0000

    Implemented sentence generation
    Only to console.log, no HTML update

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,33 @@
 //define constant arrays of wordtypes
 const adjectives = ["silly", "stupid","blue","gangly"];
-const noun = ["Joseph","Isaac","dog","bird"];
-const verb = ["jumped","ate","opened","drove","crashed out"];
-const adverb = ["happily","miserabley","angrily"];
+const nouns = ["Joseph","Isaac","dog","bird"];
+const verbs = ["jumped","ate","opened","drove","crashed out"];
+const adverbs = ["happily","miserabley","angrily"];
 
 //get references
 const genButton = document.getElementById("genButton");
 
+//event listeners
 genButton.addEventListener("click", onGenButtonClick);
 
+//functions
 function onGenButtonClick(){
-    console.log("Generate button clicked")
+    console.log("Generate button clicked");
+    //choose words
+    let chosenAdjective = adjectives[getRandomInt(adjectives.length)];
+    let chosenNoun = nouns[getRandomInt(nouns.length)];
+    let chosenVerb = verbs[getRandomInt(verbs.length)];
+    let chosenAdverb = adverbs[getRandomInt(adverbs.length)];
+    //create sentence
+    let sentence = `The ${chosenAdjective} ${chosenNoun} ${chosenVerb} ${chosenAdverb}.`;
+    console.log("Sentence generated:", sentence);
+    //update html
+
+
+    
+    
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random()*max);
 }
